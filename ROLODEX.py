b6d3ac13921d3eadd8c5b55200e3b0c24c4ea5d3
Peter Levett$ git config --global user.name Peter <peterlevett@gmail.com>
2018-01-24 20:09:22 -0800

    Added 3 simple functions in ROLODEX.py - input_entry, review_all_entries, and delete_entry_by_id.

--- ROLODEX.py
+++ ROLODEX.py
@@ -1,8 +1,24 @@
 import sqlite3
-conn = sqlite3.connect("ORDERM8.db")
-c = conn.cursor()
 
-c.execute("SELECT * FROM rolodex")
-for item in c.fetchone():
-    print item
+
+def input_entry(customerName, customerPhoneNumber, customerAddress, customerPayMethod):
+    conn = sqlite3.connect("ORDERM8.db")
+    c = conn.cursor()
+    uniqueID = 3
+    rolodexEntry = (uniqueID, customerName, customerPhoneNumber, customerAddress, customerPayMethod)
+    c.execute('INSERT INTO rolodex VALUES (?,?,?,?,?)', rolodexEntry)
+    conn.commit()
+
+def review_all_entries():
+    conn = sqlite3.connect("ORDERM8.db")
+    c = conn.cursor()
+    c.execute('SELECT * FROM rolodex')
+    for item in c:
+        print item[0], item[1], item[2], item[3], item[4]
+
+def delete_entry_by_id(uniqueID):
+    conn = sqlite3.connect("ORDERM8.db")
+    c = conn.cursor()
+    c.execute('DELETE FROM rolodex WHERE id = ?', uniqueID)
+    conn.commit()
 
