b3a5e3d05230b59ed8f05f3e2760eec111ec441c
Peter Levett$ git config --global user.name Peter <peterlevett@gmail.com>
2018-02-17 18:20:35 -0800

    gussied things up a little bit. Moved stuff around. added padding to some of the widgets.

--- ROLODEX_DISPLAY.py
+++ ROLODEX_DISPLAY.py
@@ -1,10 +1,12 @@
 from Tkinter import *
+import tkFont
 import ROLODEX
 
 
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.new_cust_font = tkFont.Font(size=30, weight='bold')
         self.grid()
         self.create_entries_and_scroll()
         self.create_input_customer()
@@ -13,9 +15,10 @@ class Application(Frame):
 
     def create_entries_and_scroll(self):
         self.scrollbar = Scrollbar(self)
-        self.text = Text(self, height=6, width=60)
-        self.scrollbar.grid(column=3, row=5)
-        self.text.grid(column=0, row=5, columnspan=2)
+        self.text = Text(self, height=6, width=80, relief=GROOVE)
+        self.entries_label = Label(self, text="Existing Customer Entries", font=self.new_cust_font).grid(row=6, columnspan=3)
+        self.scrollbar.grid(column=3, row=7,)
+        self.text.grid(column=0, row=7, columnspan=2, padx=10, pady=10)
         self.scrollbar.config(command=self.text.yview)
         self.text.config(yscrollcommand=self.scrollbar.set)
 
@@ -28,20 +31,21 @@ class Application(Frame):
         self.populate_entries()
 
     def create_input_customer(self):
-        self.name_label = Label(self, text="Name").grid(row=0)
-        self.phone_label = Label(self, text="Phone Number").grid(row=1)
-        self.address_label = Label(self, text="Address").grid(row=2)
-        self.payment_label = Label(self, text="Payment Method").grid(row=3)
+        self.input_customer_label = Label(self, text="New Customer Entry", font=self.new_cust_font).grid(row=0, columnspan=3)
+        self.name_label = Label(self, text="Name").grid(row=1)
+        self.phone_label = Label(self, text="Phone Number").grid(row=2)
+        self.address_label = Label(self, text="Address").grid(row=3)
+        self.payment_label = Label(self, text="Payment Method").grid(row=4)
         self.e1 = Entry(self)
         self.e2 = Entry(self)
         self.e3 = Entry(self)
         self.e4 = Entry(self)
-        self.e1.grid(row=0, column=1)
-        self.e2.grid(row=1, column=1)
-        self.e3.grid(row=2, column=1)
-        self.e4.grid(row=3, column=1)
-        Button(self, text="Quit", command=self.quit).grid(row=4, column=0)
-        Button(self, text="Enter Customer", command=self.input_entry).grid(row=4, column=1)
+        self.e1.grid(row=1, column=1)
+        self.e2.grid(row=2, column=1)
+        self.e3.grid(row=3, column=1)
+        self.e4.grid(row=4, column=1)
+        Button(self, text="Quit", command=self.quit).grid(row=8, column=0, columnspan=2, pady=10)
+        Button(self, text="Enter Customer", command=self.input_entry).grid(row=5, column=0, columnspan=2, pady=10)
 
     def populate_entries(self):
         self.entrylist = ROLODEX.return_all_entries()
